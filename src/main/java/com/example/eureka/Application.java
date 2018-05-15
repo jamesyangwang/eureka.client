e33e8077e74ea53f5785487e9ba7105fc7965e9d
jw88 <jw88@live.com>
2018-05-14 22:09:58 -0700

    first commit

---
+++ src/main/java/com/example/eureka/Application.java
@@ -0,0 +1,36 @@
+package com.example.eureka;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
+
+//http://localhost:8080/service-instances/eureka.client
+@RestController
+class ServiceInstanceRestController {
+	@Autowired
+	private DiscoveryClient discoveryClient;
+	
+	@RequestMapping("/service-instances/{applicationName}")
+	public List<ServiceInstance> serviceInstancesByApplicationName(
+			@PathVariable String applicationName) {
+		return this.discoveryClient.getInstances(applicationName);
+	}
+	
+}
